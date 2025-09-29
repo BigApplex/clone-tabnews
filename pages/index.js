3b6921956798d4d1990420dc0cb5fb642c1f12a3
n.Caio8 <75146855+BigApplex@users.noreply.github.com>
2025-09-29 16:05:40 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home(){
-    return <h2> Seja forte e corajoso Josué 1:9(NTLH r)</h2>; 
+    return <h2> "Por isso eu afirmo a vocês: quando vocês orarem e pedirem alguma coisa, creiam que já a receberam, e assim tudo lhes será dado."
+Marcos 11:24(NTLH)</h2>; 
 }
 
 export default Home;
