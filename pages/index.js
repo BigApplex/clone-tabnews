69881ae6a176c21bc09de6924fb850fcab0efc3a
n.Caio8 <75146855+BigApplex@users.noreply.github.com>
2025-09-29 21:45:08 +0000

    atualizado

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 function Home(){
-    return <h2> "Por isso eu afirmo a vocês: quando vocês orarem e pedirem alguma coisa, creiam que já a receberam, e assim tudo lhes será dado."
-Marcos 11:24(NTLH)</h2>; 
+    return <h1> "Por isso eu afirmo a vocês: quando vocês orarem e pedirem alguma coisa, creiam que já a receberam, e assim tudo lhes será dado."
+Marcos 11:24(NTLH)</h1>
 }
-
 export default Home;
